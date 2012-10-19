e7a0d27b6b9a32965b4d0a36f79a5e3a1d3d5bc9
unknown <DA_HE@w7-da-he.ec4u.de>
2012-10-19 13:08:40 +0200

    new set

--- Contact/ContactExplorer/gen/com/salesforce/samples/contactexplorer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.salesforce.samples.contactexplorer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
